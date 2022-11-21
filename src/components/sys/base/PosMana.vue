6ece23670e88ac3c3972e34426c13ac6af42c390
Emin <yeminx@outlook.com>
2022-11-21 16:48:41 +0800

    feat: 职位管理完善

--- src/components/sys/base/PosMana.vue
+++ src/components/sys/base/PosMana.vue
@@ -14,6 +14,7 @@
       <el-table
           :data="positions"
           border
+          @selection-change="handleSelectionChange"
           size="small"
           :header-cell-style="{textAlign:'center'}"
           :cell-style="{textAlign:'center'}"
@@ -31,17 +32,18 @@
         <el-table-column
             prop="name"
             label="职位名称"
-            width="150">
+            width="200">
         </el-table-column>
         <el-table-column
             prop="createDate"
-            label="创建时间">
+            label="创建时间"
+            width="250">
         </el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
             <el-button
                 size="mini"
-                @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                @click="showEditView(scope.$index, scope.row)">编辑</el-button>
             <el-button
                 size="mini"
                 type="danger"
@@ -49,7 +51,21 @@
           </template>
         </el-table-column>
       </el-table>
+      <el-button type="danger" size="small" style="margin-top: 8px" :disabled="multipleSelection.length===0" @click="deleteMany">批量删除</el-button>
     </div>
+    <el-dialog
+        title="修改职位"
+        :visible.sync="dialogVisible"
+        width="30%">
+      <div>
+        <el-tag>职位名称</el-tag>
+        <el-input class="updatePosInput" size="small" v-model="updatePos.name"></el-input>
+      </div>
+      <span slot="footer" class="dialog-footer">
+    <el-button size="small" @click="dialogVisible = false">取 消</el-button>
+    <el-button size="small" type="primary" @click="doUpdatePos">确 定</el-button>
+  </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -61,6 +77,11 @@ export default {
       pos: {
         name:''
       },
+      dialogVisible: false,
+      updatePos:{
+        name:'',
+      },
+      multipleSelection: [],
       positions: []
     }
   },
@@ -68,6 +89,32 @@ export default {
     this.initPositions();
   },
   methods:{
+    deleteMany(){
+      this.$confirm('此操作将永久删除【'+this.multipleSelection.length+'】条记录, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        let ids = '?';
+        this.multipleSelection.forEach(item=>{
+          ids += 'ids='+item.id + '&';
+        })
+        this.deleteRequest("/system/basic/pos/"+ids).then(resp=>{
+          if (resp){
+            this.initPositions();
+          }
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
+    },
+    handleSelectionChange(val){
+      this.multipleSelection=val
+      console.log(val)
+    },
     initPositions(){
       this.getRequest("/system/basic/pos/").then(resp=>{
         if (resp){
@@ -86,8 +133,18 @@ export default {
         this.$message.error('职位名称不可以为空');
       }
     },
-    handleEdit(index,data){
-
+    showEditView(index,data){
+      Object.assign(this.updatePos,data);
+      this.dialogVisible=true
+    },
+    doUpdatePos(){
+      this.putRequest("/system/basic/pos/",this.updatePos).then(resp=>{
+        if (resp){
+          this.initPositions();
+          this.updatePos.name='';
+          this.dialogVisible=false;
+        }
+      })
     },
     handleDelete(index,data ){
       this.$confirm('此操作将永久删除【'+data.name+'】职位, 是否继续?', '提示', {
@@ -119,5 +176,9 @@ export default {
 }
 .posManaMain{
   margin-top: 10px;
+}
+.updatePosInput{
+  width: 200px;
+  margin-left: 8px;
 }
 </style>
