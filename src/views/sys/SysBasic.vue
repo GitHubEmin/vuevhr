6a35f8945b229b766aeec8e22365ac599804be09
Emin <yeminx@outlook.com>
2022-11-22 11:22:02 +0800

    feat: 职称管理

--- src/views/sys/SysBasic.vue
+++ src/views/sys/SysBasic.vue
@@ -1,14 +1,10 @@
 <template>
   <el-tabs v-model="activeName" >
-    <el-tab-pane label="部门管理" name="first">
-      部门管理
-    </el-tab-pane>
-    <el-tab-pane label="职位管理" name="second">
-      <PosMana/>
-    </el-tab-pane>
-    <el-tab-pane label="职称管理" name="third">职称管理</el-tab-pane>
-    <el-tab-pane label="奖惩规则" name="fourth">奖惩规则</el-tab-pane>
-    <el-tab-pane label="权限组" name="fiveth">权限组</el-tab-pane>
+    <el-tab-pane label="部门管理" name="depmana"><DepMana/></el-tab-pane>
+    <el-tab-pane label="职位管理" name="posmana"><PosMana/></el-tab-pane>
+    <el-tab-pane label="职称管理" name="joblevelmana"><JobLevelMana/></el-tab-pane>
+    <el-tab-pane label="奖惩规则" name="bcmana"><BcMana/></el-tab-pane>
+    <el-tab-pane label="权限组" name="permissmana"><PermissMana/></el-tab-pane>
   </el-tabs>
 </template>
 
@@ -23,7 +19,7 @@ export default {
   name: "SysBasic",
   data() {
     return {
-      activeName:'first'
+      activeName:'joblevelmana'
     }
   },
   components:{
